e806ee5718e61f3615e17f8df5fd09c004d9df6d
SkyerWalkery <sky896142181@hotmail.com>
2022-11-09 10:36:22 +0800

    Improve error handling

--- source/main.cpp
+++ source/main.cpp
@@ -2,10 +2,13 @@
 #include <QLabel>
 #include "MainWindow.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) try{
     QApplication app(argc, argv);
     auto main_window = MainWindow();
     main_window.startGame();
     main_window.show();
     return app.exec();
+}
+catch (std::exception& e){
+    qFatal("Error %s", e.what());
 }
