77458fe2f91e0e1b2360ed482c9f50ef6cc4d117
SkyerWalkery <sky896142181@hotmail.com>
2022-11-14 15:15:04 +0800

    Optimise code structure

---
+++ source/entity/buff/BuffUtil.cpp
@@ -0,0 +1,46 @@
+#include "BuffUtil.h"
+#include <stdexcept>
+
+
+const QList<Buff>& BuffUtil::characterBuffs(){
+    static QList<Buff> character_buffs = {
+            Buff::WOLF_S_GRAVESTONE,
+            Buff::CAUSE_CORROSION,
+            /*
+            buff::INFUSION_PYRO,
+            buff::INFUSION_HYDRO,
+            buff::INFUSION_CRYO,
+            buff::INFUSION_ANEMO,
+             */
+            Buff::INFUSION_FROZEN,
+    };
+    return character_buffs;
+}
+
+QString BuffUtil::buffToIcon(Buff buff){
+    switch (buff) {
+        case Buff::WOLF_S_GRAVESTONE:
+            return ICON_DAMAGE_UP;
+        case Buff::INFUSION_FROZEN:
+            return ICON_FROZEN;
+        case Buff::CAUSE_CORROSION:
+            return ICON_CORROSION;
+        default:
+            throw std::invalid_argument("No icon for buff");
+    }
+}
+
+Buff BuffUtil::stringToBuff(const QString& str){
+    auto upper_str = str.toUpper();
+    if(upper_str == "WINDFALL")
+        return Buff::WINDFALL;
+    else if(upper_str == "EVER_CHANGING")
+        return Buff::EVER_CHANGING;
+    else
+        throw std::invalid_argument("No matching buff for given string");
+}
+
+bool BuffUtil::isDeBuff(Buff buff){
+    return buff == Buff::CORRODED || buff == Buff::FROZEN;
+}
+
