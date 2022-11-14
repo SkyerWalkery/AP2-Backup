4fd8326358cfbb30d816d1b8df86ea96c1be7a53
SkyerWalkery <sky896142181@hotmail.com>
2022-11-14 15:42:37 +0800

    Ignore test files

--- test_helpers/image_studio/clip.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-old_img = Image.open('movement_speed_up.webp')
-im = Image.new(mode="RGBA", size=(32, 32))
-# cropped = img.crop((8, 16, 72, 80))  # (left, upper, right, lower)
-im.paste(old_img, (3, 0))
-im.save("movement_speed_up.png", lossless=True, quality=100, method=6)
